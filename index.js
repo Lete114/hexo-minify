b77a93c4d59e648f12cd856f08c8f49c9714a1e6
lete114 <lete@lete114.top>
2021-08-17 15:12:17 +0800

    修改部分语法

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 /**
  * hexo-minify
  * author: Lete114
- * version: 1.0.0
+ * version: 1.0.2
  */
-'use strict'
+"use strict";
 
-var UglifyJS = require('uglify-js');
-var CleanCSS  = require('clean-css');
-var minifyHTML = require('html-minifier').minify;
+var UglifyJS = require("uglify-js");
+var CleanCSS = require("clean-css");
+var minifyHTML = require("html-minifier").minify;
 
 var config = hexo.config;
-if(config.minify==null){
-  config.minify={}
+if (!config.minify) {
+  config.minify = {
+    js: true,
+    css: true,
+    html: true,
+  };
 }
 
-hexo.extend.filter.register('after_render:js', function(str, data){
-  if(config.minify.js==true||config.minify.js==null){
-    var result = UglifyJS.minify(str);
-    return result.code;
-  }
-  return str;
+hexo.extend.filter.register("after_render:js", (str, data) => {
+  if (!config.minify.js) return str;
+  var result = UglifyJS.minify(str);
+  return result.code;
 });
 
-hexo.extend.filter.register('after_render:css', function(str, data){
-  if(config.minify.css==true||config.minify.css==null){
-    var result = new CleanCSS({}).minify(str);
-    return result.styles;
-  }
-  return str;
+hexo.extend.filter.register("after_render:css", (str, data) => {
+  if (!config.minify.css) return str;
+  var result = new CleanCSS({}).minify(str);
+  return result.styles;
 });
 
-hexo.extend.filter.register('after_render:html', function(str, data){
+hexo.extend.filter.register("after_render:html", (str, data) => {
   try {
-    if(config.minify.html==true||config.minify.html==null){
-      var result = minifyHTML(str,{
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyJS:true, 
-        minifyCSS:true
-      });
-      return result;
-    }
-    return str;
-  } catch(error) {
-    console.log('\x1B[31m%s\x1B[0m','出现异常，压缩失败！');
-    console.log('\x1B[31m%s\x1B[0m','请规范你的MarkDown语法！');
-    console.log('\x1B[33m%s\x1B[0m','检查是否是“"”的问题，错误如下！');
+    if (!config.minify.html) return str;
+    var result = minifyHTML(str, {
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCSS: true,
+    });
+    return result;
+  } catch (error) {
+    console.log("\x1B[31m%s\x1B[0m", "出现异常，压缩失败！");
+    console.log("\x1B[31m%s\x1B[0m", "请规范你的MarkDown语法！");
+    console.log("\x1B[33m%s\x1B[0m", '检查是否是“"”的问题，错误如下！');
     console.log(error);
     return str;
   }
